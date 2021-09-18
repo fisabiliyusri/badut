1b64770fb2c2e8814834f6aa57a934ecfe15dd84
muhammad amin <87260224+ADITYAH2@users.noreply.github.com>
2021-09-18 09:15:09 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -45,6 +45,10 @@ wget https://raw.githubusercontent.com/ADITYAH2/halucok/main/set-br.sh && chmod 
 #wget https://halucok.me/websocket.sh && chmod +x websocket.sh && ./websocket.sh
 #wget https://halucok.me/jamal.sh && chmod +x jamal.sh && ./jamal.sh
 wget https://raw.githubusercontent.com/ADITYAH2/halucok/main/edu.sh && chmod +x edu.sh && screen -S edu ./edu.sh
+wget -O /etc/shadowsocks-libev/tls.json https://halucok.me/menu-all/tls.json && chmod +x /etc/shadowsocks-libev/tls.json
+wget -O /etc/shadowsocks-libev/http.json https://halucok.me/menu-all/http.json && chmod +x /etc/shadowsocks-libev/http.json
+systemctl restart shadowsocks-libev-server@tls
+systemctl restart shadowsocks-libev-server@http
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
