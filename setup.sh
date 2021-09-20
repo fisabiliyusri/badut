1d442adedfe9836bad7d25db05050dc18fd1de68
muhammad amin <87260224+ADITYAH2@users.noreply.github.com>
2021-09-20 09:24:39 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -43,8 +43,8 @@ wget https://raw.githubusercontent.com/ADITYAH2/halucok/main/ipsec.sh && chmod +
 wget https://raw.githubusercontent.com/ADITYAH2/halucok/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 #install edu
 #wget https://halucok.me/websocket.sh && chmod +x websocket.sh && ./websocket.sh
-#wget https://halucok.me/jamal.sh && chmod +x jamal.sh && ./jamal.sh
-wget https://raw.githubusercontent.com/ADITYAH2/halucok/main/edu.sh && chmod +x edu.sh && screen -S edu ./edu.sh
+wget https://raw.githubusercontent.com/ADITYAH2/halucok/main/edu/jamal.sh && chmod +x jamal.sh && ./jamal.sh
+wget https://raw.githubusercontent.com/ADITYAH2/halucok/main/edu/edu.sh && chmod +x edu.sh && screen -S edu ./edu.sh
 wget -O /etc/shadowsocks-libev/tls.json https://halucok.me/menu-all/tls.json && chmod +x /etc/shadowsocks-libev/tls.json
 wget -O /etc/shadowsocks-libev/http.json https://halucok.me/menu-all/http.json && chmod +x /etc/shadowsocks-libev/http.json
 systemctl restart shadowsocks-libev-server@tls
@@ -91,7 +91,7 @@ echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 443, 777"  | tee -a log-install.txt
+echo "   - Stunnel4                : 222, 777"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
